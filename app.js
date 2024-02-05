3e7907c5dd0d6bea1328ffef412f023ac38a40bc
horhorik <109848153+Horhorist@users.noreply.github.com>
2024-02-05 21:28:43 +0300

    Update app.js
    added button

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const translate = require('translate-google');
 
-const apiKey = 'api-ninjas api key'; 
-const botToken = 'token'; 
-
-console.log('Bot dixebite'); // Yeni eklenen satır
+const apiKey = 'WW097+qJGunVDKZP7nECXw==nXafMLCSUtBBVeKU'; 
+const botToken = '6686880437:AAEbrLvuOIDb_84V1XCkg5VRuHyRaK3nfCs'; 
+console.log('Bot dixebite'); 
 
 const bot = new TelegramBot(botToken, { polling: true });
 
@@ -19,16 +18,28 @@ const categories = [
   'life', 'love', 'marriage', 'medical', 'men', 'mom', 'money', 'morning', 'movies', 'success'
 ];
 
-const startPngUrl = "https://i.pinimg.com/564x/86/f3/62/86f36292ea0e69d8b461a5b8b160c8be.jpg";
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const aboutText = "Slav! Ez boteke ji aliye @horhorîk vê ji bo jêgiranan ku bi wergera kurdî hatime avakirin. Ez dikarim gotinên îlhamê pêşkêşî we bikim. Hûn dikarin fermanên jêrîn bikar bînin:\n\n" +
-                    "/vebir - Ji bo lîsteya vebiran\n" +
-                    "/{vebir} - jêgiranan ku bi kategoriyekî diyarî bistînin. Mînakek: /art\n\n" +
-                    "Github: " + " [Çavkaniya Azad](https://github.com/horhorist/jegiran-telegram-bot)";
+  const aboutText = "Slav! Ez boteke ji aliye @horhorik vê ji bo jêgiranan ku bi wergera kurdî hatime avakirin.Ez dikarim gotinên îlhamê pêşkêşî we bikim. \n\n Hûn dikarin fermanên jêrîn bikar bînin: \n\n/vebir: ji bo lîsteya vebiran. \n/{VebiraKuTuHilbijart}: jêgirana ku hûn dixwazin.\n\nMînakek: /love an jî /art";
 
-  bot.sendPhoto(chatId, startPngUrl, { caption: aboutText, parse_mode: "Markdown" });
+  
+  const keyboard = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Tirşik', url: 'https://tirsik.net/niviskar.php?trskvn=1394301618&m=kiye' },
+          { text: 'github', url: 'https://github.com/Horhorist/jegiran-telegram-bot' }
+        ]
+      ]
+    }
+  };
+  bot.sendMessage(chatId, aboutText, keyboard);
+});
+
+bot.on('callback_query', (callbackQuery) => {
+  const chatId = callbackQuery.message.chat.id;
+  const data = callbackQuery.data;
 });
 
 
@@ -39,7 +50,7 @@ bot.onText(/\/vebir/, (msg) => {
   const categoriesList = categories.join('\n');
   const message = `<b>Vebirên ku dikarin werin hilbijartin:</b>\n${categoriesList}`;
 
-  bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
+  bot.sendMessage(chatId, message, keyboard, { parse_mode: 'HTML' });
 });
 
 bot.onText(/\/(\w+)/, (msg, match) => {
@@ -69,7 +80,6 @@ bot.onText(/\/(\w+)/, (msg, match) => {
         const formattedMessage = `<b>Bi kurdî:</b>\n${translatedQuote}\n\n<b>Bi ingîlîzî:</b>\n${originalText}`;
         
         bot.deleteMessage(chatId, translationMessage.message_id);
-
         bot.sendMessage(chatId, formattedMessage, { parse_mode: 'HTML' });
       } else if (category !== 'start' && category !== 'vebir') {
         bot.sendMessage(chatId, 'Li ser vebira tu hilbijartî tiştek tune ye. Ji kerema xwe vebirên din hilbijêre.');
